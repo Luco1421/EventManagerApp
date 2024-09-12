c5b5b4d5e18ac005f188d76e70984275a1fb7892
luco1421 <byluco1421@gmail.com>
2024-09-12 12:59:28 -0600

    first version of the controller

--- src/main/java/com/example/dbii/controller/CRUD.java
+++
@@ -1,4 +0,0 @@
-package com.example.dbii.controller;
-
-public class CRUD {
-}
---
+++ src/main/java/com/example/dbii/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.dbii.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/")
+    public String getLogin() {
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String getHome() {
+        return "home";
+    }
+
+
+}
